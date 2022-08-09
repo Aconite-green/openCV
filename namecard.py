36eff7eec7470dd3f641d6da4e965d5202da5f22
SeongYong <pjii50260@naver.com>
2022-08-09 23:27:18 +0900

    Update namecard.py

--- namecard.py
+++ namecard.py
@@ -15,6 +15,14 @@ src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 #threshold
 th ,src_bin = cv2.threshold(src_gray, 0, 255, cv2.THRESH_BINARY or cv2.THRESH_OTSU)
 
+#외곽선 검출
+countours,_ = cv2.findContours(src_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+#print(len(countours)) 
+
+for pts in countours:
+    if cv2.contourArea(pts) < 1000:
+        continue
+    cv2.polylines(src, pts, True, (0,0,255))
 
 cv2.imshow('src', src)
 cv2.imshow('src_gray', src_gray)
